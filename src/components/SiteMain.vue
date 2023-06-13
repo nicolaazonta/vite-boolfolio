e8b6c91546103e28f58a3cf9c89cc300936327a6
nicolaazonta <nicolazonta114@gmail.com>
2023-06-13 17:06:11 +0200

    fighting with vue & vite

---
+++ src/components/SiteMain.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from 
+
+export default {
+    name: "HelloWorld",
+}
+</script>
+
+<template>
+    <div class="container w-75">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+
+                </div>
+            </div>
+        </div>
+        <div class="card"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
